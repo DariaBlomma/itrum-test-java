b45da7261507a44d210a13427b8b0f253a81e5c9
Daria Kozina <daria.kozina@uco.co.il>
2026-03-17 20:47:39 +0300

    added timeout exceptions

--- src/main/java/com/wallet/itrumtestjava/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/wallet/itrumtestjava/exceptions/GlobalExceptionHandler.java
@@ -1,35 +1,54 @@
 package com.wallet.itrumtestjava.exceptions;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.dao.DataAccessResourceFailureException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.TransactionTimedOutException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
 import java.time.Instant;
+
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleNotFound(ResourceNotFoundException ex, HttpServletRequest request) {
-        return buildErrorResponse(HttpStatus.NOT_FOUND, ex, request);
+        return buildErrorResponse(HttpStatus.NOT_FOUND, ex.getMessage(), request);
     }
 
     @ExceptionHandler(InvalidRequestException.class)
     public ResponseEntity<ErrorResponse> handleBadRequest(InvalidRequestException ex, HttpServletRequest request) {
-        return buildErrorResponse(HttpStatus.BAD_REQUEST, ex, request);
+        return buildErrorResponse(HttpStatus.BAD_REQUEST, ex.getMessage(), request);
+    }
+
+    @ExceptionHandler(InsufficientFundsException.class)
+    public ResponseEntity<ErrorResponse> handleInsufficientFunds(InsufficientFundsException ex, HttpServletRequest request) {
+        return buildErrorResponse(HttpStatus.BAD_REQUEST, ex.getMessage(), request);
+    }
+
+    @ExceptionHandler(TransactionTimedOutException.class)
+    public ResponseEntity<ErrorResponse> handleTransactionTimedOut(TransactionTimedOutException ex, HttpServletRequest request) {
+        String message = "Transaction timeout. The server is busy, please try again later.";
+        return buildErrorResponse(HttpStatus.SERVICE_UNAVAILABLE, message, request);
+    }
+
+    @ExceptionHandler(DataAccessResourceFailureException.class)
+    public ResponseEntity<ErrorResponse> handleConnectionTimedOut(DataAccessResourceFailureException ex, HttpServletRequest request) {
+        String message = "Database connection timeout. Could not get connection from pool, please try again.";
+        return buildErrorResponse(HttpStatus.SERVICE_UNAVAILABLE, message, request);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleGeneric(Exception ex, HttpServletRequest request) {
        ex.printStackTrace();
-       return buildErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, ex, request);
+       return buildErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage(), request);
     }
 
-    private ResponseEntity<ErrorResponse> buildErrorResponse(HttpStatus status, Exception ex, HttpServletRequest request) {
+    private ResponseEntity<ErrorResponse> buildErrorResponse(HttpStatus status, String message, HttpServletRequest request) {
         ErrorResponse error = new ErrorResponse(
                 status.value(),
-                ex.getMessage(),
+                message,
                 Instant.now(),
                 request.getRequestURI()
         );
